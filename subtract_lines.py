b8f36547ea23309550d11220ea60f45111729126
Leonid Beynenson <Leonid.Beynenson@itseez.com>
2016-06-30 21:19:59 +0300

    Added useful scripts subtract_lines.py and ddete

---
+++ subtract_lines.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import time
+import re
+import sys
+import datetime
+import math
+
+first_line = None
+last_line = None
+
+for line in sys.stdin:
+    if last_line:
+        print last_line,
+
+    if not first_line:
+        first_line = line
+
+    last_line = line
+
+date_re = re.compile(r'^[0-9-_]+')
+
+first_date_match = date_re.match(first_line)
+last_date_match = date_re.match(last_line)
+
+if first_date_match and last_date_match:
+    first_date = first_date_match.group()
+    last_date = last_date_match.group()
+
+    first_time = datetime.datetime.strptime(first_date, "%Y-%m-%d_%H-%M-%S")
+    last_time = datetime.datetime.strptime(last_date, "%Y-%m-%d_%H-%M-%S")
+
+    dt_time = last_time - first_time
+
+    numsec = int(dt_time.total_seconds())
+    nummin = int(math.ceil( (numsec + 10.0) / 60.0))
+    print "rest {} min".format(nummin)
+
+print last_line,
