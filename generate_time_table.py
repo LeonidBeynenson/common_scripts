277478ee00bfb986eee7debdf6f22ce77a1496b0
Leonid Beynenson <Leonid.Beynenson@gmail.com>
2021-09-29 21:18:15 +0300

    Add generate_time_table.py

---
+++ generate_time_table.py
@@ -0,0 +1,9 @@
+from datetime import date, timedelta
+sdate = date(2021, 4, 1)
+edate = date(2021, 12, 31)
+delta = edate - sdate
+for i in range(delta.days + 1):
+    cur_date = sdate + timedelta(days=i)
+    weekday = cur_date.strftime('%a')
+    target_time_in_hours = 5.5 if weekday not in ('Sat', 'Sun') else 0.0
+    print(f"'{cur_date}': {target_time_in_hours}")
